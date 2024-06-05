4e91a720609bb05fab49ad12888cf3a6361a64e3
Dhanam2305 <167000726+Dhanam2305@users.noreply.github.com>
2024-06-05 12:03:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router,  Route, Routes } from "react-router-dom";
+import { Home }  from "./components/Pages/Home";
+import { About } from "./components/Pages/About";
+import { Blog } from "./components/Pages/Blog";
+import { Contact } from "./components/Pages/Contact";
+
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <NavBar />
+
+        <div className="pages">
+          <Routes>
+            
+            <Route path="/" element={<Home />} />
+            
+            <Route path="/about" element={<About />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+            
+          </Routes>
+        </div>
+      </Router>
+  </>
+  );
+}
+
+export default App;
